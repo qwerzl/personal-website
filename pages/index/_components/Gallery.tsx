8c7a76e5e99e9852a2bebddaaea926acaa7e1d40
qwerzl <me@qwerzl.me>
2023-11-21 11:53:09 +0800

    feat: sticky heading initial test

--- pages/index/_components/Gallery.tsx
+++ pages/index/_components/Gallery.tsx
@@ -15,7 +15,7 @@ export default () => {
     onMount(() => {
         if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)){
             // @ts-ignore
-            setOffsetTop(header.offsetTop-convertRemToPixels(1.7))
+            setOffsetTop(header.offsetTop-convertRemToPixels(5))
             // Safari and Chromium seem to behave differently on offsetTop...
         } else {
             // @ts-ignore
@@ -26,14 +26,21 @@ export default () => {
     const ws = createScrollPosition()
     return (
         <>
-            <h2 class="flex items-center mb-4 font-semibold text-3xl"
+            <style>{`
+                .hidden {
+                    display: none;
+                }
+            `}</style>
+            <h2 class="flex items-center mb-4 font-semibold text-3xl mt-14"
                 classList={{
-                    ["fixed top-0 mt-10 items-center mb-4 w-full"]: offsetTop()<(ws.y),
-                    ["mt-14"]: offsetTop()>(ws.y)
+                    //["fixed top-0 mt-10 items-center mb-4 w-full"]: offsetTop()<(ws.y),
+                    //["mt-14"]: offsetTop()>(ws.y)
+                    ["hidden"]: offsetTop()<(ws.y)
                 }}
                 ref={header}
                 onClick={openLink}
             >
+
                 <span flex-1 class="title text-white">
                     Popular Photos
                 </span>
@@ -43,6 +50,24 @@ export default () => {
                     <div class="m-2 i-ri-arrow-right-up-line text-white" onClick={openLink}></div>
                 </div>
             </h2>
+
+            <h2 class="flex items-center mb-4 font-semibold text-3xl fixed top-0 mt-10 items-center mb-4 w-full"
+                classList={{
+                    ["hidden"]: offsetTop()>(ws.y)
+                }}
+                onClick={openLink}
+            >
+
+                <span flex-1 class="title text-white">
+                    Popular Photos
+                </span>
+                <div
+                    class="op-50 ml-2 hover:op-100 transition-opacity cursor-pointer"
+                >
+                    <div class="m-2 i-ri-arrow-right-up-line text-white" onClick={openLink}></div>
+                </div>
+            </h2>
+
             <Masonry />
 
 
